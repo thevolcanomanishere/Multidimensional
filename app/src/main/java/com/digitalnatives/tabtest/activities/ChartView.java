18961b8bc26a6e385885ae9eff45752b65188e83
thevolcanomanishere <alexmcgon209@gmail.com>
2016-04-20 17:47:13 +0100

    added some unversioned files.

---
+++ app/src/main/java/com/digitalnatives/tabtest/activities/ChartView.java
@@ -0,0 +1,15 @@
+package com.digitalnatives.tabtest.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.digitalnatives.tabtest.R;
+
+public class ChartView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart_view);
+    }
+}
